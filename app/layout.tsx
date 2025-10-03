fe4e07c00f0b62911f1653489ca1169d2f359696
TgPraise <olakunlet949@gmail.com>
2025-10-03 01:10:53 +0100

    landing page

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { GeistSans } from "geist/font/sans"
+import { GeistMono } from "geist/font/mono"
+import { Analytics } from "@vercel/analytics/next"
+import { Suspense } from "react"
+import "./globals.css"
+
+export const metadata: Metadata = {
+  title: "ZirraPay - Asset exchange",
+  description: "Cryptocurrency Transaction platform for seamless crypto-to-naira Exchange",
+  authors: [{ name: "Temitope Olakunle - TG Praise" }],
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+        <Suspense fallback={null}>{children}</Suspense>
+        <Analytics />
+      </body>
+    </html>
+  )
+}
